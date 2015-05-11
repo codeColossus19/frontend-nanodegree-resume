2436cd7f77b2b88b1dde130cf6b7582946cea7e0
td <tddemiche@gmail.com>
2015-05-10 23:20:33 -0700

    chained string replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,18 @@
 var yourName="CodeColossus19";
 var role="Director of Clandestine Operations";
+var bio={
+	"Name":yourName,
+	"Role":role,
+	"Contacts":{
+		"Mobile": "123-4655",
+		"Morse Code" : "Vun Zero",
+		"Cell"  : "867-309",
+		"Email" : "jibberish@aol.com"
+	},
+	"PictureUrl": "C:/Git/frontend-nanodegree-resume/images/subzero.jpg",
+	"WelcomeMessage":"Welcome to my fantabulous site!",
+	"Skills":"Chess, Eating, Chipotle, Awesomeness"	
+}
 
 // var awesomeThoughts="I am "+yourName+" and I am AWESOME!";
 // console.log(awesomeThoughts);
@@ -7,9 +20,29 @@ var role="Director of Clandestine Operations";
 // var funThoughts=awesomeThoughts.replace("AWESOME","fun");
 
 // $("#main").append(funThoughts);
+//var contactKeys=Object.getOwnPropertyNames(bio.Contacts);
 
-var formattedName=HTMLheaderName.replace('%data%',yourName);
-var formattedRole=HTMLheaderRole.replace('%data%',role);
+var formattedName=HTMLheaderName.replace('%data%',bio.Name);
+var formattedRole=HTMLheaderRole.replace('%data%',bio.Role);
 
-$('#header').prepend(formattedRole);
-$('#header').prepend(formattedName);
+var formattedContacts;
+
+for(var key in bio.Contacts)
+{
+	formattedContacts=HTMLcontactGeneric.replace("%contact%",key+":").replace("%data%",bio.Contacts[key]);
+	
+	$('#footerContacts').append(formattedContacts);
+}
+
+var formattedPic=HTMLbioPic.replace("%data%",bio.PictureUrl);
+var formattedWelcomeMessage=HTMLwelcomeMsg.replace("%data%",bio.WelcomeMessage)
+var formattedSkills=HTMLskills.replace("%data%", bio.Skills);
+
+
+$('#header').append(formattedName);
+$('#header').append(formattedRole);
+$('#header').append(formattedPic);
+$('#header').append(formattedWelcomeMessage);
+$('#header').append(formattedSkills);
+
+
