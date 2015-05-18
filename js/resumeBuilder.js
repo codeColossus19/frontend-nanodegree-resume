b5af63d0c1d6586bc9047103ee3e3bda47b3faa1
tdemichele_rules <tdemichele@godaddy.com>
2015-05-18 08:33:53 -0700

    begin lesson 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,18 +1,66 @@
-var yourName="CodeColossus19";
-var role="Director of Clandestine Operations";
 var bio={
-	"Name":yourName,
-	"Role":role,
-	"Contacts":{
-		"Mobile": "123-4655",
-		"Morse Code" : "Vun Zero",
-		"Cell"  : "867-309",
-		"Email" : "jibberish@aol.com"
-	},
-	"PictureUrl": "C:/Git/frontend-nanodegree-resume/images/subzero.jpg",
-	"WelcomeMessage":"Welcome to my fantabulous site!",
-	"Skills":"Chess, Eating, Chipotle, Awesomeness"	
-}
+    "Name": "CodeColossus19",
+    "Role": "Director of Clandestine Operations",
+    "Contacts": {
+        "Mobile": "123-4655",
+        "Morse Code": "Vun Zero",
+        "Cell": "8675-309",
+        "Email": "jibberish@aol.com"
+    },
+    "PictureUrl": "C:/Git/frontend-nanodegree-resume/images/subzero.jpg",
+    "WelcomeMessage": "Welcome to my fantabulous site!",
+    "Skills": "Chess, Eating, Chipotle, Awesomeness"
+};
+
+var work= {
+    "jobposition": "Commander",
+    "employer": "Lin Quai Dojo",
+    "years": 4,
+    "City": "Phoenix"
+};
+
+
+var education = {
+    "schools": [
+        {
+            "name": "Arizona State University",
+            "city": "Tempe, Arizona",
+            "degree": "BA",
+            "major": "Computer Information Systems"
+        },
+        {
+            "name": "Arcadia High School",
+            "city": "Phoenix, Arizona",
+            "degree": "Diploma",
+            "major": "High School"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Javascript Basics",
+            "school": "Udacity",
+            "dates": 2014,
+            "url": "https://www.udacity.com/course/viewer#!/c-ud804"
+        }
+    ]
+};
+
+var projects = {
+    "project": [
+        {
+            "title": "Javascript Course",
+            "CourseFormat": "Online",
+            "dates": 2015,
+            "description": "Javascript Basics"
+        },
+        {
+            "title": "C# Course",
+            "CourseFormat": "Self Study",
+            "dates": 2015,
+            "description": "C# expert skills"
+        }
+    ]
+};
 
 // var awesomeThoughts="I am "+yourName+" and I am AWESOME!";
 // console.log(awesomeThoughts);
@@ -21,7 +69,7 @@ var bio={
 
 // $("#main").append(funThoughts);
 //var contactKeys=Object.getOwnPropertyNames(bio.Contacts);
-
+/*
 var formattedName=HTMLheaderName.replace('%data%',bio.Name);
 var formattedRole=HTMLheaderRole.replace('%data%',bio.Role);
 
@@ -36,13 +84,35 @@ for(var key in bio.Contacts)
 
 var formattedPic=HTMLbioPic.replace("%data%",bio.PictureUrl);
 var formattedWelcomeMessage=HTMLwelcomeMsg.replace("%data%",bio.WelcomeMessage)
-var formattedSkills=HTMLskills.replace("%data%", bio.Skills);
+
+var formattedUniversity=HTMLschoolName.replace("%data%",education.onlineCourses[0].title);
+var formattedWorkPosition=HTMLworkTitle.replace("%data%",work.jobposition);
 
 
+//header
 $('#header').append(formattedName);
 $('#header').append(formattedRole);
 $('#header').append(formattedPic);
 $('#header').append(formattedWelcomeMessage);
+
+
+//projects
+
+
+//work experience
+$('#workExperience').append(formattedWorkPosition);
+
+//education
+$('#education').append(formattedUniversity);
+
+//mapDiv
+*/
+
+if(bio.skills!="")
+{
+var formattedSkills=HTMLskills.replace("%data%", bio.Skills);
 $('#header').append(formattedSkills);
+$('#skills').append(formattedSkills);
+}
 
 
